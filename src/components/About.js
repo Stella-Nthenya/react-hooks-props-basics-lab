70cde8b51c2d6c9b57932042b785e64c95044d67
Stella Nthenya <stellaalogi@gmail.com>
2023-06-02 22:25:12 +0300

    final

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      {bio&& !bio== "" ? <p>{props.bio}</p> : null}
+      {bio&& !bio=== "" ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       <Links github={props.github} linkedin={props.linkedin}/>
     </div>
