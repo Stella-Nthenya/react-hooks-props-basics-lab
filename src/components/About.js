ac6093c6e4bd1958561fdcc27cda6fd01b574954
Stella Nthenya <stellaalogi@gmail.com>
2023-06-02 22:23:33 +0300

    checked if bio is an empty string

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>{bio}</p>
+      {bio&& !bio== "" ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       <Links github={props.github} linkedin={props.linkedin}/>
     </div>
